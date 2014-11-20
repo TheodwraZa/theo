45c436f5ebfd8a8af72e033e3b9b9965704989af
θεοδωρα ζαχαρουδη < @Dwra-VAIO6>
2014-11-20 11:53:17 +0200

    new commit

---
+++ src/theo/theo_class.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package theo;
+
+/**
+ *
+ * @author θεοδωρα ζαχαρουδη
+ */
+public class theo_class {
+    
+        public class theo {
+        public void print(){
+        System.out.println("god");
+        } 
+}
+}
+
+    
